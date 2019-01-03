8f763b297dc10eb0b27d2b247ca045cdc1ee5a3b
Lucy Wall <lhwall@gmail.com>
2019-01-03 14:41:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ return document.querySelector( '#nested .target')
 
 function increaseRankBy(n){
   const amount = parseInt(n)
-<<<<<<< HEAD
   const doc = document.querySelectorAll("ul.ranked-list")
 
   for (let i = 0; i < doc.length; i++){
     doc[i] += amount
-=======
+
   const doc = document.querySelector(".ranked-list")
 
   return for (let i = 0; i < doc.length; i++){
     doc[i] += amount
-    
->>>>>>> 193583ace733a8ce5409a4aff959fe16bf2b704d
+  
   }
 }
